2a05e4c73b282cd499e0129d88b496d7f78a800c
Jadwiga <matuzalema@users.noreply.github.com>
2018-01-04 14:26:31 +0100

    Add files via upload

---
+++ RandomNumberMachine/script.js
@@ -0,0 +1,59 @@
+const randomButton = document.getElementById("losuj");
+const randomNumberCircle = document.getElementsByClassName("number");
+const clearButton = document.getElementById("clear");
+var result = [];
+
+function rysuj(){
+	 //zerowanie
+ 	var dupa = document.getElementsByClassName('number');
+ 	$(".number").remove();
+ for (var i = 0; i < result.length; i++) {
+
+
+ 	const newRandomNumber = document.createElement('div');
+
+	newRandomNumber.textContent = result[i];
+	newRandomNumber.classList.add("number");
+	
+	const section = document.querySelector('section');
+	section.appendChild(newRandomNumber);
+ };
+
+
+}
+
+
+function losowanie(){
+	if (result.length===6){
+		return;
+	}
+	
+	const wynikLosowania = Math.floor(Math.random()*49+1);
+
+	for(let i=0; i<result.length; i++){
+		if(wynikLosowania === result[i])
+			return losowanie();
+	}
+
+
+	result.push(wynikLosowania);
+
+ rysuj()
+
+	
+};
+
+
+
+randomButton.addEventListener("click", losowanie);
+
+clearButton.addEventListener("click", function(){
+	result=[];
+	rysuj();
+	console.log("dupa");
+});
+
+	
+
+
+
